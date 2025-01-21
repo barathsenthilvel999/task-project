2a8802533ac3665c0cd684b1eb24be989694bdca
barathsenthilvel <barath.senthilvel@twilightsoftwares.com>
2025-01-21 18:36:17 +0530

    feat(init): Initial commit with Laravel setup

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'), // Use Laravel's bcrypt for hashing
+            'role' => 'admin',
+        ]);
+ 
+        User::create([
+            'name' => 'Author User',
+            'email' => 'author@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'author',
+        ]);
+
+        User::create([
+            'name' => 'Reviewer User',
+            'email' => 'reviewer@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'reviewer',
+        ]);
+    }
+}
